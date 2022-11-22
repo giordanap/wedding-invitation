779a88010c99c61e373fcc20bfc459bacbe498cb
admravisgitea <yrodrigp@everis.com>
2022-11-22 01:40:03 -0500

    formulario WHATSAPP 4

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function sendData() {
     const nombres = document.getElementById("nombres").value;
     const apellidos = document.getElementById("apellidos").value;
     
-    const text = `Hola%20Giordan%20&%20Patricia,%20soy%20${nombres}%20 %20${apellidos}.%20¡CONFIRMO%20MI%20ASISTENCIA!`
+    const text = `Hola%20Giordan%20&%20Patricia,%20soy%20${nombres}%20${apellidos}.%20¡CONFIRMO%20MI%20ASISTENCIA!`
     window.location.href = `https://wa.me/931017921?text=${text}`;
 }
